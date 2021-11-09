00fadea65e50a7fbc9450b7b2d759e1a0bb8c15a
“leonshimizu” <lmshimizu@gmail.com>
2021-11-08 18:46:24 -0800

    used webpack to load index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log("Hello from JS");
 
+var moment = require('moment');
+
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-const darkmode = new Darkmode();
-darkmode.toggle();
-console.log(darkmode.isActivated()); // will return true
+// const darkmode = new Darkmode();
+// darkmode.toggle();
+// console.log(darkmode.isActivated()); // will return true
