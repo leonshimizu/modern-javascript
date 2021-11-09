63a59d1e706cbce55c9c430304b1beaf1035f684
“leonshimizu” <lmshimizu@gmail.com>
2021-11-08 22:25:57 -0800

    testing progress watch

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ console.log(`Hello World - from ${name}`);
 console.log(`Hello ${name}, right now it is ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
 
 console.log("TEST");
+
+name = "Bob";
+console.log("Hello World - from ".concat(name));
+console.log("Hello ".concat(name, ", right now it is ").concat(moment().format('MMMM Do YYYY, h:mm:ss a')));
